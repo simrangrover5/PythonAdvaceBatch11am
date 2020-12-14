47e9be0055774dde3eed20cd3c18b2b64672994e
simrangrover5 <simrangrover5@gmail.com>
2020-12-14 12:44:50 +0530

    return html pages

--- django_project/blogproject/app1/views.py
+++ django_project/blogproject/app1/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is APP1 App")
+    # return HttpResponse("This is APP1 App")
+    return render(request, "one.html")
+    # templates --> one.html
+    # templates --> html --> one.html
+    # render(request, "html/one.html")
+
+
+# create project with name my project
+# create 2 apps with name users, blog
+# return index.html page when request for localhost
+# create form in django
